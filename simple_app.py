e34a25921a6ba1871bad8fa9cc25fc3ee226ddba
AhmedElmougy <ahmed.m.elmogy@gmail.com>
2021-03-03 17:57:35 +0200

    add todo model

--- simple_app.py
+++ simple_app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
-list = [
-    {
-        'desc': 'todo 1'
-    },
-    {
-        'desc': 'todo 2'
-    },
-    {
-        'desc': 'todo 3'
-    },
-    {
-        'desc': 'todo 4'
-    },
-]
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://user@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    todo_item = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.todo_item}>'
+
+
+db.create_all()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=list)
+    return render_template('index.html', data=Todo.query.all())
